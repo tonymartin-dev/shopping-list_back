0f6a2af14aa7e8b5d11b0a891576644a8715dbf1
marijose <guerreromjosefa@gmail>
2023-01-22 02:44:36 +0100

    basic CRUD api

--- routes/lists.ts
+++ routes/lists.ts
@@ -1,20 +1,58 @@
-import express from "express";
-import { List } from "../schemas/list";
+import express from "express"
+import { checkList, ListModel } from "../schemas/list"
 
-const listsRouter = express.Router();
+const listsRouter = express.Router()
 
 listsRouter.get('/', function(req, res, next) {
-  List.find({})
+  ListModel.find({})
   .exec((err, story) => {
-    //if (err) return handleError(err);
+    //if (err) return handleError(err)
     res.status(200)
-    res.send(story);
-  });
-});
+    res.send(story)
+  })
+})
 
-listsRouter.get('/', function(req, res, next) {
-  console.log("BODY", req.body);
-  
+listsRouter.post('/', async (req, res, next) => {
+  try{
+    const newList = new ListModel(checkList(req.body))
+    await newList.save()
+    res.send(newList)
+  } catch (e) {
+    const error = e as (Error & {code: number})
+    res.status(400)
+    if (error.code === 11000 || error.code === 11001) {
+      res.status(409).send({error: 'You can\'t repeat list names.'})
+    }
+    res.send({error: error.toString()})
+  }
+})
+
+listsRouter.put('/', async (req, res, next) => {
+  try {
+    const newList = checkList(req.body)
+    const savedList = await ListModel.updateOne(
+      { _id: newList.id },
+      newList,
+      {
+        runValidators: true
+      }
+    )
+    res.send(savedList)
+  } catch(e) {
+    res.send({error: (e as Error).toString()})
+  }
+})
+
+listsRouter.delete('/', async (req, res, next) => {
+  try {
+    const _id = req.body.id
+    await ListModel.deleteOne({ _id })
+    res.send({message: "List succesfully deleted"})
+  } catch(e) {
+    res.send({error: (e as Error).toString()})
+  }
+
+
 })
 
 
--- schemas/list.ts
+++ schemas/list.ts
@@ -1,10 +1,41 @@
 import mongoose from "mongoose";
 
+export interface List {
+  name: string,
+  items: string[]
+}
+
+export interface SavedList extends List {
+  id: string
+}
+
 const Schema = mongoose.Schema;
 
-const listSchema = new Schema({
+const listSchema = new Schema<List>({
   name: String,
   items: Array<String>
 });
 
-export const List = mongoose.model("List", listSchema);
+export const ListModel = mongoose.model("List", listSchema);
+
+export const checkList = (list: unknown, isSaved?: boolean) => {
+  const checkedList = list as SavedList;
+
+  if(!checkedList?.name || typeof checkedList.name !== "string") {
+    throw new Error("Field 'name' is not properly formatted")
+  }
+
+  if(
+    !checkedList?.items || 
+    !Array.isArray(checkedList.items) || 
+    checkedList.items.some(item => typeof item !== "string")
+  ){
+    throw new Error("Field 'items' is not properly formatted")
+  }
+
+  if(isSaved && !checkedList.id){
+    throw new Error("Field 'id' is required")
+  }
+
+  return checkedList
+}
