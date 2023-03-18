bea54ef5ab52a0b95b69d06fc32164da72893ac3
tonymartin-dev <hello@tonymartin-web.com>
2023-03-18 12:12:45 +0100

    Separate controllers from routes

---
+++ src/controllers/lists.ts
@@ -0,0 +1,52 @@
+import { checkList, ListModel } from "../schemas/list"
+import { Request, Response, NextFunction } from 'express';
+
+export const getByIdController = async (req: Request, res: Response, _next: NextFunction) => {
+    const lists = await ListModel.find({})
+    res.status(200).send(lists)
+}
+
+export const getAllController = async (req: Request, res: Response, _next: NextFunction) => {
+    try {
+        const newList = new ListModel(checkList(req.body))
+        await newList.save()
+        res.send(newList)
+    } catch (e) {
+        const error = e as (Error & { code: number })
+        if (error.code === 11000 || error.code === 11001) {
+            res.status(400).send({ error: 'You can\'t repeat list names.' })
+            return
+        }
+        res.status(400).send({ error: error.toString() })
+    }
+}
+
+export const updateController = async (req: Request, res: Response, _next: NextFunction) => {
+    try {
+        const newList = checkList(req.body)
+        await ListModel.updateOne(
+            { _id: newList.id },
+            newList,
+            { runValidators: true }
+        )
+        res.send(req.body)
+    } catch (e) {
+        res.status(400).send({ error: (e as Error).toString() })
+    }
+}
+
+export const deleteController = async (req: Request, res: Response, _next: NextFunction) => {
+    try {
+        const _id = req.body.id
+        if (typeof _id !== "string") {
+            throw Error("Invalid value for 'id' property.")
+        }
+
+        await ListModel.deleteOne({ _id })
+        res.send({ message: "List succesfully deleted" })
+    } catch (e) {
+        res.status(400).send({ error: (e as Error).toString() })
+    }
+
+
+}
--- src/routes/lists.ts
+++ src/routes/lists.ts
@@ -1,63 +1,12 @@
 import express from "express"
-import { checkList, ListModel } from "../schemas/list"
+import { deleteController, getAllController, getByIdController, updateController } from "../controllers/lists"
 
 const listsRouter = express.Router()
 
-listsRouter.get('/', async (req, res, next) => {  
-  const lists = await ListModel.find({})  
-  res.status(200).send(lists)
-})
-
-listsRouter.get('/list/:id', async (req, res, next) => {
-  const _id = req.params.id  
-  const list = await ListModel.findOne({_id})
-  res.status(200).send(list)
-})
-
-listsRouter.post('/', async (req, res, next) => {
-  try{
-    const newList = new ListModel(checkList(req.body))
-    await newList.save()
-    res.send(newList)
-  } catch (e) {
-    const error = e as (Error & {code: number})
-    if (error.code === 11000 || error.code === 11001) {
-      res.status(400).send({error: 'You can\'t repeat list names.'})
-      return
-    }
-    res.status(400).send({error: error.toString()})
-  }
-})
-
-listsRouter.put('/', async (req, res, next) => {
-  try {
-    const newList = checkList(req.body)
-    await ListModel.updateOne(
-      { _id: newList.id },
-      newList,
-      { runValidators: true }
-    )
-    res.send(req.body)
-  } catch(e) {
-    res.status(400).send({error: (e as Error).toString()})
-  }
-})
-
-listsRouter.delete('/', async (req, res, next) => {
-  try {
-    const _id = req.body.id
-    if(typeof _id !== "string") {
-      throw Error("Invalid value for 'id' property.")
-    }
-    
-    await ListModel.deleteOne({ _id })
-    res.send({message: "List succesfully deleted"})
-  } catch(e) {
-    res.status(400).send({error: (e as Error).toString()})
-  }
-
-
-})
-
+listsRouter.get('/', )
+listsRouter.get('/list/:id', getByIdController)
+listsRouter.post('/', getAllController)
+listsRouter.put('/', updateController)
+listsRouter.delete('/', deleteController)
 
 export default listsRouter
